3c0b4ccd083f71ad9e7e62c93670e0bd92253811
weishenai <59504283+weishenai@users.noreply.github.com>
2023-12-14 22:31:03 +0800

    feat:新增自动加载entities

--- src/common/database/dbConfig.ts
+++ src/common/database/dbConfig.ts
@@ -6,15 +6,15 @@ export function buildConnectionOptions(configService: ConfigService) {
   const config = configService.get('DATABASE');
   const logFlag = configService.get('LOG_CONFIG')['LOG_ON'] === 'true';
 
-  // 扫描文件夹下所有 mysql.entity.ts 后缀结尾的文件
-  const entitiesDir =
-    process.env.NODE_ENV === 'env.local'
-      ? [join(__dirname, 'entities') + '/**/*.entity.{js,ts}']
-      : [join(__dirname, 'entities') + '/**/*.entity.{js,ts}'];
-  console.log('entitiesDir', entitiesDir);
+  // 扫描文件夹下所有 mysql.entity.ts 后缀结尾的文件 （TODO: 改为自动搜索）
+  // const entitiesDir =
+  //   process.env.NODE_ENV === 'env.local'
+  //     ? [join(__dirname, 'entities') + '/**/*.entity.{js,ts}']
+  //     : [join(__dirname, 'entities') + '/**/*.entity.{js,ts}'];
+  // console.log('entitiesDir', entitiesDir);
   return {
     ...config,
-    entities: entitiesDir,
+    autoLoadEntities: true,
     logging: logFlag,
   } as TypeOrmModuleOptions;
 }
