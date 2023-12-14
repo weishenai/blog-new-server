3e070f971ffc1092cbfa4c5254bfe689eba30103
weishenai <59504283+weishenai@users.noreply.github.com>
2023-12-14 22:54:18 +0800

    feat: category,comment 相关 controller补充

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Delete, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) { }
+
+  @Post("/add")
+  async add() { }
+
+  @Put("/update")
+  async update() { }
+
+  @Delete("/delete")
+  async delete() { }
+
+  @Post("/getCategoryList")
+  async getCategoryList() { }
+
+  @Get("/getCategoryDictionary")
+  async getCategoryDictionary() { }
 }
--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Delete,Post, Put } from '@nestjs/common';
 import { CommentService } from './comment.service';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @Post("/add")
+  async add(){}
+
+  @Post("/apply")
+  async apply(){}
+
+  @Put("/thumbUp/:id")
+  async thumbUpComment(){}
+
+  @Put("/cancelThumbUp/:id")
+  async cancelThumbUp(){}
+
+  @Delete("/delete/:id/:parent_id")
+  async deleteComment(){}
+
+  @Delete("/backDelete/:id/:parent_id")
+  async deleteCommentByServer(){}
+
+  @Post("/backGetCommentList")
+  async backGetCommentList(){}
+
+  @Post("/frontGetParentComment")
+  async frontGetParentComment(){}
+
+  @Post("/frontGetChildrenComment")
+  async frontGetChildrenComment(){}
+
+  @Post("/getCommentTotal")
+  async getCommentTotal(){}
 }
